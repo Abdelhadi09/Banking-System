e7bb3475ef88311fe1c6219e3ebb6cd3c999bbca
abdelhadi <abaditrki@gmail.com>
2024-11-03 20:24:09 -0800

    adding the bank class

---
+++ src/Bank.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Bank {
+    private List<Customer> customers;
+
+    public Bank() {
+        customers = new ArrayList<>();
+    }
+
+    public void addCustomer(Customer customer) {
+        customers.add(customer);
+    }
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+}
